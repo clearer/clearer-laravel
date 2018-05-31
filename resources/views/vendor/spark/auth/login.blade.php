d6c6202424f4eda6e9de4b16a72516ec9dc1d5de
Josh Mobley <josh@joshmobley.net>
2018-05-31 18:44:28 -0400

    starting to style login

--- resources/views/vendor/spark/auth/login.blade.php
+++ resources/views/vendor/spark/auth/login.blade.php
@@ -1,61 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-lg-8">
-            <div class="card card-default">
-                <div class="card-header">{{__('Login')}}</div>
 
-                <div class="card-body">
-                    @include('spark::shared.errors')
+    @include('spark::shared.errors')
 
-                    <form role="form" method="POST" action="/login">
-                        {{ csrf_field() }}
-
-                        <!-- E-Mail Address -->
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label text-md-right">{{__('E-Mail')}}</label>
-
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}" autofocus>
-                            </div>
-                        </div>
-
-                        <!-- Password -->
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label text-md-right">{{__('Password')}}</label>
-
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
-                            </div>
-                        </div>
-
-                        <!-- Remember Me -->
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <label class="form-check-label">
-                                        <input type="checkbox" name="remember" class="form-check-input"> {{__('Remember Me')}}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Login Button -->
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{__('Login')}}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">{{__('Forgot Your Password?')}}</a>
-                            </div>
-                        </div>
-                    </form>
+    <div class="cards">
+        <div class="card" style="background: white">
+            <form role="form" method="POST" class="form" action="/login">
+                
+                {{ csrf_field() }}
+            
+                <div class="form__group">
+                <!-- E-Mail Address -->
+                    <label>{{__('E-Mail')}}</label>
+                    <input type="email" name="email" value="{{ old('email') }}" autofocus>
                 </div>
-            </div>
+            
+                <div class="form__group">
+                <!-- Password -->
+                    <label>{{__('Password')}}</label>
+                    <input type="password" name="password">
+                </div>
+            
+                <div class="form__group">
+                <!-- Remember Me -->
+                    <label>
+                        <input type="checkbox" name="remember"> {{__('Remember Me')}}
+                    </label>
+                </div>
+            
+                <div class="form__group">
+                <!-- Login Button -->
+                    <button class="button" type="submit">
+                        {{__('Login')}}
+                    </button>
+                </div>
+            
+                <a href="{{ url('/password/reset') }}">{{__('Forgot Your Password?')}}</a>
+            </form>
         </div>
     </div>
-</div>
 @endsection
